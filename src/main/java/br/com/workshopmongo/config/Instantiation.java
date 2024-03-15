876344040ea5ee8bf1ed8cb69da4d5b03c4cde20
Jonas Avelino <jonasavma@gmail.com>
2024-03-14 22:30:04 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/br/com/workshopmongo/config/Instantiation.java
@@ -0,0 +1,32 @@
+package br.com.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.workshopmongo.domain.User;
+import br.com.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		userRepository.deleteAll();
+		// instacia os objetos e salvar no banco de dados
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+	}
+
+}
