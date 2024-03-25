ae32347dded5ec0ebaa0787edb24805f6af679e7
Jonas Avelino <jonasavma@gmail.com>
2024-03-25 00:19:48 -0300

    Obtendo um post por id

---
+++ src/main/java/br/com/workshopmongo/resources/PostResources.java
@@ -0,0 +1,27 @@
+package br.com.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.workshopmongo.domain.Post;
+import br.com.workshopmongo.service.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResources {
+
+	@Autowired
+	private PostService postService;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
